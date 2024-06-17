915185e8529844b2c6832855fad935650011b9f5
william <william.dai@student.com>
2024-06-17 13:39:43 +0800

    feat: return user info on wishlist

--- wishlist/serializer.py
+++ wishlist/serializer.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
+from account.serializer import UserSerializer
 from wishlist.models import Wishlist
 
 
 class WishlistSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
     class Meta:
         model = Wishlist
         fields = '__all__'
