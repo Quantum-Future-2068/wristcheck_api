4e93f093ceb3c7d37d8deb8dd861152bfc1626a7
william <daixiang1992@gmail.com>
2024-08-08 21:42:18 +0800

    feat: stage mysql 5

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -5,7 +5,7 @@ from environs import Env
 env = Env()
 env.read_env(override=False)
 
-bind = f'{env.str("HOST", "0.0.0.0")}:{env.str("PORT", 80)}'
+bind = f'{env.str("HOST", "0.0.0.0")}:{env.str("API_PORT", 8888)}'
 workers = env.int("GUNICORN_WORKERS", multiprocessing.cpu_count() * 2 + 1)
 threads = 1
 timeout = 30
@@ -15,5 +15,5 @@ wsgi_app = "wristcheck_api.wsgi:application"
 preload_app = True
 proc_name = "wristcheck_api"
 loglevel = env.str("GUNICORN_LOG_LEVEL", "debug")
-accesslog = env.str("GUNICORN_ACCESS_LOG", "/var/log/gunicorn_access.log")
-errorlog = env.str("GUNICORN_ERROR_LOG", "/var/log/gunicorn_error.log")
+# accesslog = env.str("GUNICORN_ACCESS_LOG", "/var/log/gunicorn_access.log")
+# errorlog = env.str("GUNICORN_ERROR_LOG", "/var/log/gunicorn_error.log")
